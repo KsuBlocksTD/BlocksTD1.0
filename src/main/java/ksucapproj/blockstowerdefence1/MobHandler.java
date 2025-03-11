fac6ed20c2b20b560fd50e4b4d8a942266a21f24
Logan Slicker <52476041+SlickerLogan@users.noreply.github.com>
2025-03-11 00:16:50 -0400

    Delete src/main/java/ksucapproj/blockstowerdefence1 directory

--- src/main/java/ksucapproj/blockstowerdefence1/MobHandler.java
+++
@@ -1,9 +0,0 @@
-package ksucapproj.blockstowerdefence1;
-
-import org.bukkit.Location;
-import org.bukkit.event.Listener;
-
-public class MobHandler implements Listener {
-    public static void spawnMob(Location world) {
-    }
-}
