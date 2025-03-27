9abdd9ab05a8706ce57ada443f2aa6dd66b1a686
Matthew Elledge <mtelledge14@gmail.com>
2025-03-27 01:03:02 -0400

    3/27 1:02 AM PlayerSword Upgrades working with Sweeping Edge and Material Upgrades
    -- No longer requires sword in-hand--
    
    Signed-off-by: Matthew Elledge <mtelledge14@gmail.com>

--- src/main/java/ksucapproj/blockstowerdefense1/logic/game_logic/PlayerSword.java
+++ src/main/java/ksucapproj/blockstowerdefense1/logic/game_logic/PlayerSword.java
@@ -6,22 +6,28 @@ import ksucapproj.blockstowerdefense1.ConfigOptions;
 import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.attribute.Attribute;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.UUID;
 
 public class PlayerSword {
 
     private int swordLevel, slownessLevel, sweepingEdgeLevel;
+
     private final Player player;
     private ItemStack playerSword;
     private ItemMeta swordMeta;
+    private final String swordUUID;
 
 
     private static final PartiesAPI api = BlocksTowerDefense1.getApi();
@@ -30,21 +36,17 @@ public class PlayerSword {
 
     public PlayerSword(Player player){
 
+        // all sword attributes initialized before being applied to an upgrade mechanic
         this.player = player;
         this.slownessLevel = 0;
         this.sweepingEdgeLevel = 0;
         this.swordLevel = 1;
+
+        // sword attributes instantiated before being passed to createPlayerSword()
         this.playerSword = new ItemStack(Material.WOODEN_SWORD);
         this.swordMeta = playerSword.getItemMeta();
-
-        swordMeta.displayName(Component.text(ChatColor.GOLD + "Tower Defense Sword"));
-        swordMeta.setUnbreakable(true);
-        swordMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
-        playerSword.setItemMeta(swordMeta);
+        this.swordUUID = createPlayerSword(playerSword, swordMeta);
     }
-
-
-
 
 
 
@@ -53,10 +55,9 @@ public class PlayerSword {
 
         if (upgrades.swordLevel < config.getSwordMaterialMaxLevel()){
 
-            upgrades.setSwordLevel(upgrades.swordLevel++);
+            upgrades.setSwordLevel(++upgrades.swordLevel);
         }
     }
-
 
 
     public static void applySlownessUpgrade(PlayerSword upgrades) {
@@ -64,7 +65,7 @@ public class PlayerSword {
         // Apply slowness to the mob (target)
         if (upgrades.slownessLevel < config.getSlownessMaxLevel()){
 
-            upgrades.slownessLevel++; // increase the level
+            ++upgrades.slownessLevel; // increase the level
 
         }
 
@@ -87,9 +88,7 @@ public class PlayerSword {
 
         if (upgrades.sweepingEdgeLevel < config.getSweepingEdgeMaxLevel()){
 
-            upgrades.setSweepingEdgeLevel(upgrades.sweepingEdgeLevel++); // increase the level
-
-
+            upgrades.setSweepingEdgeLevel(++upgrades.sweepingEdgeLevel); // increase the level
         }
     }
 
@@ -108,6 +107,7 @@ public class PlayerSword {
 
     public void setSwordLevel(int swordLevel) {
 
+        // based upon the player's sword level, it initializes the newSwordMaterial as it
         Material newSwordMaterial = switch (swordLevel) {
             case 1 -> Material.WOODEN_SWORD;
             case 2 -> Material.STONE_SWORD;
@@ -119,9 +119,14 @@ public class PlayerSword {
 
         swordMeta = playerSword.getItemMeta();
 
-        player.getInventory().removeItem(player.getItemInHand());
+        removeTrackedSword();
+
         playerSword = new ItemStack(newSwordMaterial);
+
+        NamespacedKey key = new NamespacedKey(BlocksTowerDefense1.getInstance(), "sword_id");
+        swordMeta.getPersistentDataContainer().set(key, PersistentDataType.STRING, swordUUID);
         playerSword.setItemMeta(swordMeta);
+
         player.getInventory().addItem(playerSword);
     }
 
@@ -134,19 +139,68 @@ public class PlayerSword {
     // THIS IS WORKING PROPERLY
     // MODEL ALL SWORD UPGRADES OFF THIS
     public void setSweepingEdgeLevel(int sweepingEdgeLevel) {
-        this.sweepingEdgeLevel = sweepingEdgeLevel;
+        this.sweepingEdgeLevel = sweepingEdgeLevel; // sets PlayerSword sweepingEdgeLevel as parameter taken one
 
-        swordMeta = playerSword.getItemMeta();
+        swordMeta = playerSword.getItemMeta(); // stores playerSword's meta-data to be retrieved later
 
-        if (swordMeta.hasConflictingEnchant(Enchantment.SWEEPING_EDGE)){
+        if (swordMeta.hasConflictingEnchant(Enchantment.SWEEPING_EDGE)){ // if the sword has the enchant first
             swordMeta.removeEnchant(Enchantment.SWEEPING_EDGE);
         }
 
         swordMeta.addEnchant(Enchantment.SWEEPING_EDGE, sweepingEdgeLevel, true);
         playerSword.setItemMeta(swordMeta);
 
-        player.getInventory().removeItem(player.getItemInHand());
+        removeTrackedSword();
+
         player.getInventory().addItem(playerSword);
+    }
+
+
+    // This properly creates the sword with a trackable ID for deletion when being modified
+    private String createPlayerSword(ItemStack playerSword, ItemMeta swordMeta){
+
+        // Gives the sword the basic information and needs when first handed to the player
+        swordMeta.displayName(Component.text(ChatColor.GOLD + "Tower Defense Sword")); // sword name-tag
+        swordMeta.setUnbreakable(true); // makes the sword unbreakable
+        swordMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE); // the sword will not show its unbreakable status
+
+        // this is the key to later identify the sword by later
+        NamespacedKey key = new NamespacedKey(BlocksTowerDefense1.getInstance(), "sword_id");
+        PersistentDataContainer data = swordMeta.getPersistentDataContainer();
+        String swordUUID = player.getUniqueId() + "-" + UUID.randomUUID(); // creates a random UUID for the sword when created
+        data.set(key, PersistentDataType.STRING, swordUUID); // stores the swordUUID into the sword's persistent meta-data
+
+        playerSword.setItemMeta(swordMeta); // this applies all changes done to swordMeta to the playerSword
+
+        player.getInventory().addItem(playerSword); // gives the player the sword
+
+        return swordUUID; // used for initializing the UUID in the PlayerSword constructor
+    }
+
+
+    private void removeTrackedSword(){
+        if (swordUUID == null) return; // if the player doesn't have a sword, it cannot be removed
+
+        NamespacedKey key = new NamespacedKey(BlocksTowerDefense1.getInstance(), "sword_id");
+
+        // variables created outside of scope to prevent unnecessary creation
+        PersistentDataContainer data;
+        String itemUUID;
+
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item == null || !item.hasItemMeta()) continue; // if inv slots are empty or don't have meta-data they are skipped
+
+            data = item.getItemMeta().getPersistentDataContainer();
+            if (data.has(key, PersistentDataType.STRING)) {
+
+                itemUUID = data.get(key, PersistentDataType.STRING); // if the item's itemUUID matches sword_id, it removes the item from inv
+
+                if (swordUUID.equals(itemUUID)) {
+                    player.getInventory().remove(item);
+                    break; // break the loop after deleting the sword
+                }
+            }
+        }
     }
 
 }
