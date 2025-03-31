d6ea04c95d1ca1f818b2c1c276670d2da8b7e348
Matthew Elledge <mtelledge14@gmail.com>
2025-03-31 15:32:03 -0400

    Renamed server in config.yml to btd
    Signed-off-by: Matthew Elledge <mtelledge14@gmail.com>

--- src/main/java/ksucapproj/blockstowerdefense1/ConfigOptions.java
+++ src/main/java/ksucapproj/blockstowerdefense1/ConfigOptions.java
@@ -25,93 +25,93 @@ public class ConfigOptions {
 
     // Getter for Speed level max upgrade
     public int getSpeedMaxLevel() {
-        return config.getInt("server.attributes.upgrades.player.speed.max-level", 5);  // Default value is 5 if not set
+        return config.getInt("btd.attributes.upgrades.player.speed.max-level", 5);  // Default value is 5 if not set
     }
     public int getSpeedBaseCost() {
-        return config.getInt("server.attributes.upgrades.player.speed.cost", 500); // Default value is 500 if not set
+        return config.getInt("btd.attributes.upgrades.player.speed.cost", 500); // Default value is 500 if not set
     }
 
 
     // Getter for Slowness level max upgrade
     public int getSlownessMaxLevel() {
-        return config.getInt("server.attributes.upgrades.player.sword.slowness.max-level", 5);  // Default value is 5 if not set
+        return config.getInt("btd.attributes.upgrades.player.sword.slowness.max-level", 5);  // Default value is 5 if not set
     }
     public int getSlownessBaseCost(){
-        return config.getInt("server.attributes.upgrades.player.sword.slowness.cost", 400); // Default value is 400 if not set
+        return config.getInt("btd.attributes.upgrades.player.sword.slowness.cost", 400); // Default value is 400 if not set
     }
     public int getSlownessDuration() {
-        return config.getInt("server.attributes.upgrades.player.sword.slowness.duration", 10); // Default value is 10 (ticks) if not set
+        return config.getInt("btd.attributes.upgrades.player.sword.slowness.duration", 10); // Default value is 10 (ticks) if not set
     }
 
     public boolean getSlownessDurationIncreaseOnUpgrade() {
-        return config.getBoolean("server.attributes.upgrades.player.sword.slowness.duration.increase-duration-on-upgrade", false); // Default value is false if not set
+        return config.getBoolean("btd.attributes.upgrades.player.sword.slowness.duration.increase-duration-on-upgrade", false); // Default value is false if not set
     }
 
 
     // Getter for Strength level max upgrade
     public int getStrengthMaxLevel() {
-        return config.getInt("server.attributes.upgrades.player.strength.max-level", 5);  // Default value is 5 if not set
+        return config.getInt("btd.attributes.upgrades.player.strength.max-level", 5);  // Default value is 5 if not set
     }
     public int getStrengthBaseCost() {
-        return config.getInt("server.attributes.upgrades.player.strength.cost", 750);
+        return config.getInt("btd.attributes.upgrades.player.strength.cost", 750);
     }
 
 
     // Getter for Sweeping-Edge level max upgrade
     public int getSweepingEdgeMaxLevel() {
-        return config.getInt("server.attributes.upgrades.player.sword.sweeping-edge.max-level", 5);  // Default value is 5 if not set
+        return config.getInt("btd.attributes.upgrades.player.sword.sweeping-edge.max-level", 5);  // Default value is 5 if not set
     }
     public int getSweepingEdgeBaseCost() {
-        return config.getInt("server.attributes.upgrades.player.sword.sweeping-edge.cost", 400);  // Default value is 600 if not set
+        return config.getInt("btd.attributes.upgrades.player.sword.sweeping-edge.cost", 400);  // Default value is 600 if not set
     }
 
 
     public int getSwordMaterialMaxLevel() {
         // 1: wood, 2: stone, 3: iron, 4: diamond, 5: netherite
-        return config.getInt("server.attributes.upgrades.player.sword.material.max-level", 5);  // Default value is 5 if not set
+        return config.getInt("btd.attributes.upgrades.player.sword.material.max-level", 5);  // Default value is 5 if not set
     }
     public int getSwordMaterialBaseCost() {
         // 1: wood, 2: stone, 3: iron, 4: diamond, 5: netherite
-        return config.getInt("server.attributes.upgrades.player.sword.material.cost", 400);  // Default value is 400 if not set
+        return config.getInt("btd.attributes.upgrades.player.sword.material.cost", 400);  // Default value is 400 if not set
     }
 
 
 
     // Getter for the message of the day for the server
     public String getMOTDOnPlayerJoin(){
-        if (config.getBoolean("server.settings.motd.enable", false)){
-            return config.getString("server.settings.motd.message");
+        if (config.getBoolean("btd.settings.motd.enable", false)){
+            return config.getString("btd.settings.motd.message");
         }
         return null;
     }
 
     // Getter for the greeting of a player on join-in
     public String getGreetOnPlayerJoin(){
-        if (config.getBoolean("server.settings.greet-player.enable", true)){
-            return config.getString("server.settings.greet-player.message");
+        if (config.getBoolean("btd.settings.greet-player.enable", true)){
+            return config.getString("btd.settings.greet-player.message");
         }
         return null;
     }
 
 
     public int getBasicTowerCost(){
-        return config.getInt("server.attributes.upgrades.towers.basic.cost", 500);
+        return config.getInt("btd.attributes.upgrades.towers.basic.cost", 500);
     }
 
     public int getFastTowerCost(){
-        return config.getInt("server.attributes.upgrades.towers.fast.cost", 300);
+        return config.getInt("btd.attributes.upgrades.towers.fast.cost", 300);
     }
 
     public int getSniperTowerCost(){
-        return config.getInt("server.attributes.upgrades.towers.sniper.cost", 750);
+        return config.getInt("btd.attributes.upgrades.towers.sniper.cost", 750);
     }
 
     public int getSplashTowerCost(){
-        return config.getInt("server.attributes.upgrades.towers.splash.cost", 900);
+        return config.getInt("btd.attributes.upgrades.towers.splash.cost", 900);
     }
 
     public int getSlowTowerCost(){
-        return config.getInt("server.attributes.upgrades.towers.slow.cost", 450);
+        return config.getInt("btd.attributes.upgrades.towers.slow.cost", 450);
     }
 
 }
